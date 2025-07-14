14d803daab7a78d58401fae771d42af1a19229b2
Cecilia19991029 <zzhang.cecilia@gmail.com>
2025-07-14 13:46:12 -0500

    Initial commit for ReMarket frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import RegisterForm from './components/RegisterForm';
+import ProductList from './components/ProductList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+      <h1>ReMarket</h1>
+      <RegisterForm />
+      <ProductList />
     </div>
   );
 }
 
 export default App;
+
